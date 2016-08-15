6b55878fbafd7f4ee4cd566ff4df58b6f7770e18
Jesse Vincent <jesse@bestpractical.com>
2016-08-15 12:57:47 -0700

    rename macros for currently unused configuration registers

--- firmware/wire-protocol.h
+++ firmware/wire-protocol.h
@@ -33,25 +33,25 @@ extern uint8_t device_config;
  * 01: Auto clear INT in 5ms
  * 10: Auto clear INT in 10ms
  */
-#define ISSI_CONFIG_ACI(CFG) (((CFG) >> 5) & 0x03)
+#define DEVICE_CONFIG_ACI(CFG) (((CFG) >> 5) & 0x03)
 
 /* DE: Input Port Filter Enable
  * 0: Input port filter disable
  * 1: Input port filter enable
  */
-#define ISSI_CONFIG_DE(CFG)  (((CFG) >> 4) & 0x01)
+#define DEVICE_CONFIG_DE(CFG)  (((CFG) >> 4) & 0x01)
 
 /* SD: Key Scan Debounce Time
  * 0: Double debounce time (6ms, 8ms), 14ms total
  * 1: Normal debounce time (3ms, 4ms), 7ms total
  */
-#define ISSI_CONFIG_SD(CFG)  (((CFG) >> 3) & 0x01)
+#define DEVICE_CONFIG_SD(CFG)  (((CFG) >> 3) & 0x01)
 
 /* LE: Long-pressed Key Detect Enable
  * 0: Disable
  * 1: Enable
  */
-#define ISSI_CONFIG_LE(CFG)  (((CFG) >> 2) & 0x01)
+#define DEVICE_CONFIG_LE(CFG)  (((CFG) >> 2) & 0x01)
 
 /* LT: Long-pressed Key Detect Delay Time:
  * 00: 20ms
@@ -59,7 +59,7 @@ extern uint8_t device_config;
  * 10: 1s
  * 11: 2s
  */
-#define ISSI_CONFIG_LT(CFG)  ((CFG) & 0x03)
+#define DEVICE_CONFIG_LT(CFG)  ((CFG) & 0x03)
 
 // I²C driver functions
 void twi_data_received( uint8_t *buf, uint8_t bufsiz);
