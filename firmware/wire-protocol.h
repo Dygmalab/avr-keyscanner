7b94362dc60e693f5a06e6d646743e7774ad5b71
Jesse Vincent <jesse@bestpractical.com>
2016-08-17 15:30:12 -0700

    Remove ISSI-compatible single-opcode configuration
    Removing in favor of something chattier, but more extensible

--- firmware/wire-protocol.h
+++ firmware/wire-protocol.h
@@ -30,40 +30,6 @@ typedef union {
 // Configuration register
 extern uint8_t device_config;
 
-
-/* ACI: Auto Clear INT
- * 00: Auto clear INT disabled
- * 01: Auto clear INT in 5ms
- * 10: Auto clear INT in 10ms
- */
-#define DEVICE_CONFIG_ACI(CFG) (((CFG) >> 5) & 0x03)
-
-/* DE: Input Port Filter Enable
- * 0: Input port filter disable
- * 1: Input port filter enable
- */
-#define DEVICE_CONFIG_DE(CFG)  (((CFG) >> 4) & 0x01)
-
-/* SD: Key Scan Debounce Time
- * 0: Double debounce time (6ms, 8ms), 14ms total
- * 1: Normal debounce time (3ms, 4ms), 7ms total
- */
-#define DEVICE_CONFIG_SD(CFG)  (((CFG) >> 3) & 0x01)
-
-/* LE: Long-pressed Key Detect Enable
- * 0: Disable
- * 1: Enable
- */
-#define DEVICE_CONFIG_LE(CFG)  (((CFG) >> 2) & 0x01)
-
-/* LT: Long-pressed Key Detect Delay Time:
- * 00: 20ms
- * 01: 40ms
- * 10: 1s
- * 11: 2s
- */
-#define DEVICE_CONFIG_LT(CFG)  ((CFG) & 0x03)
-
 // I²C driver functions
 void twi_data_received( uint8_t *buf, uint8_t bufsiz);
 void twi_data_requested( uint8_t *buf, uint8_t *bufsiz);
