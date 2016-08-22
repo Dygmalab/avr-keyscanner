c53c9d0e8bf0f4085cbd2c8661b368f6b0e80a12
Jesse Vincent <jesse@bestpractical.com>
2016-08-21 23:39:11 -0700

    Reorder the SPI frequencies so that the lowest number is the slowest speed

--- firmware/wire-protocol-constants.h
+++ firmware/wire-protocol-constants.h
@@ -10,14 +10,14 @@
 #define TWI_CMD_COLS_USE_PULLUPS 0x07
 #define TWI_CMD_LED_SPI_FREQUENCY 0x08
 
-#define LED_SPI_OFF                 0x00
-#define LED_SPI_FREQUENCY_4MHZ      0x01
-#define LED_SPI_FREQUENCY_2MHZ      0x02
-#define LED_SPI_FREQUENCY_1MHZ      0x03
+#define LED_SPI_FREQUENCY_4MHZ      0x07
+#define LED_SPI_FREQUENCY_2MHZ      0x06
+#define LED_SPI_FREQUENCY_1MHZ      0x05
 #define LED_SPI_FREQUENCY_512KHZ    0x04
-#define LED_SPI_FREQUENCY_256KHZ    0x05
-#define LED_SPI_FREQUENCY_128KHZ    0x06
-#define LED_SPI_FREQUENCY_64KHZ     0x07
+#define LED_SPI_FREQUENCY_256KHZ    0x03
+#define LED_SPI_FREQUENCY_128KHZ    0x02
+#define LED_SPI_FREQUENCY_64KHZ     0x01
+#define LED_SPI_OFF                 0x00
 
 
 // 512KHZ seems to be the sweet spot in early testing
