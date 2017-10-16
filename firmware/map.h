7593c75008db4e91bccbb75b6b3ed12bc5f3197f
matt venn <matt@mattvenn.net>
2017-10-16 16:42:15 +0200

    updating map for chinese leds

--- firmware/map.h
+++ firmware/map.h
@@ -1,12 +1,13 @@
 #pragma once
 #define XXX 0xFF 
+// farnell leds are different rgb order to chinese, which is why most triplets have first 2 rows reversed
+// made a mistake on the pcb and swapped leds l4 and i4, these are starting with 150 and 153, which is why they are reversed above
 const uint8_t led_LUT[2][128] = {
-    { XXX,    XXX,    0,   3,   7,   9,   12,  15,  18,  21,  24,  27,  30,  33,  36,  39,  
-      XXX,    XXX,    1,   4,   6,   10,  13,  16,  19,  22,  25,  28,  31,  34,  37,  40, 
+    { XXX,    XXX,    0,   3,   7,   10,  13,  15,  18,  22,  25,  28,  31,  34,  37,  40,  
+      XXX,    XXX,    1,   4,   6,   9,   12,  16,  19,  21,  24,  27,  30,  33,  36,  39, 
       XXX,    XXX,    2,   5,   8,   11,  14,  17,  20,  23,  26,  29,  32,  35,  38,  41,  
-                                                                                                                                      
-      42,  45,  48,  XXX,    XXX,    51,  54,  57,  60,  63,  66,  69,  72,  75,  78,  81,  
-      43,  46,  49,  XXX,    XXX,    52,  55,  58,  61,  64,  67,  70,  73,  76,  79,  82,  
+      43,  46,  48,  XXX,    XXX,    51,  54,  57,  60,  63,  66,  69,  72,  75,  78,  81,  
+      42,  45,  49,  XXX,    XXX,    52,  55,  58,  61,  64,  67,  70,  73,  76,  79,  82,  
       44,  47,  50,  XXX,    XXX,    53,  56,  59,  62,  65,  68,  71,  74,  77,  80,  83,  
                                                                                                                                       
       84,  87,  90,  93,  96,  99,  XXX,    XXX,    102, 105, 108, 111, 114, 117, 120, 123, 
@@ -23,5 +24,4 @@ const uint8_t led_LUT[2][128] = {
       XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    
       XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    XXX,    
   } };
-// made a mistake on the pcb and swapped leds l4 and i4, these are starting with 150 and 153, which is why they are reversed above
 
