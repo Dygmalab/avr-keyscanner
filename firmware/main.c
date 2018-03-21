cfb207767a7b368db218f33f43cf6dde09c035fb
matt venn <matt@mattvenn.net>
2018-03-21 14:12:10 +0100

    fix adc settings

--- firmware/main.c
+++ firmware/main.c
@@ -14,28 +14,26 @@
 void setup_adc()
 {
     PRR &= ~(1<< PRADC); //write logic one to shutdown, zero to wake
+    ADMUX =
+        (0 << ADLAR) |     // don't left shift result
+        (0 << REFS0) |     // Sets ref. voltage to 1.1v
+        (0 << MUX3)  |     // use ADC6 for input
+        (1 << MUX2)  |
+        (1 << MUX1)  |
+        (0 << MUX0);
 
-    //ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (0 << ADPS0); // clock divided by 64 125 kHz
-   // ADCSRA |= (1 << ADATE); // setting auto trigger
-//  ADCSRB |= (0 << ADTS2) | (0 << ADTS1) | (0 << ADTS0); // free running mode - default
-//  ADMUX |= (0 << REFS0); // ref voltage is Vcc - default
-//  ADMUX |= (0 << ADLAR); // using 10 bit resolution - default
-
-    // I want adc7 - pa1 on matrix board mux[3:0] - 0111
-    ADMUX &= ~(1 << MUX3);
-    ADMUX |=  (1 << MUX2);
-    ADMUX |=  (1 << MUX1);
-    ADMUX |=  (1 << MUX0);
-
-    _delay_us(200); // wait for 25 ADC cycles at 125kHz
-
-    ADCSRA |= (1 << ADEN); // enable ADC
-    ADCSRA |= (1 << ADSC);  // start sampling free running mode
+    ADCSRA = 
+        (1 << ADEN)  |     // Enable ADC 
+        (1 << ADPS2) |     // set prescaler to 64, bit 2 
+        (1 << ADPS1) |     // set prescaler to 64, bit 1 
+        (0 << ADPS0);      // set prescaler to 64, bit 0  
 }
 
 
 uint16_t read_adc()
 {
+    ADCSRA |= (1 << ADSC);         // start ADC measurement
+    while (ADCSRA & (1 << ADSC) ); // wait till conversion complete 
     uint8_t low = ADCL;
     uint8_t high = ADCH;
     uint16_t result = high;
@@ -65,11 +63,13 @@ static inline void setup(void) {
 
     #endif
     #ifdef DETECT_ADC
+    DDRA &= ~_BV(1); // setup as input
+    PORTA |= _BV(1); // pull down
 
     setup_adc();
     while(true)
     {
-        if(read_adc() > 500)
+        if(read_adc() < 500)
             led_set_all_to( red );
         else
             led_set_all_to( grn );
