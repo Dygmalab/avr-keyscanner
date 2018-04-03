d82db0d447b7c98422b37dee7c4d9e0d7976d6c5
Jesse Vincent <jesse@keyboard.io>
2018-04-02 20:32:07 -0700

    Add back the alternate config where the rows are outputs and the cols are inputs

--- firmware/config/attiny88.h
+++ firmware/config/attiny88.h
@@ -9,6 +9,8 @@
  *
  */
 
+#define COLS_ARE_OUTPUTS
+
 // ROWS
 #define PORT_ROWS PORTC
 #define DDR_ROWS DDRC
@@ -24,6 +26,8 @@
 #define COL_COUNT 8
 
 
+#ifdef COLS_ARE_OUTPUTS
+
 //Scanning port (rows)
 #define PORT_INPUT PORT_ROWS
 #define DDR_INPUT DDR_ROWS
@@ -37,6 +41,24 @@
 #define PIN_OUTPUT PIN_COLS
 #define OUTPUT_PINMASK COL_PINMASK
 #define OUTPUT_COUNT COL_COUNT
+
+#else
+
+//Signal port (rows)
+#define PORT_OUTPUT PORT_ROWS
+#define DDR_OUTPUT DDR_ROWS
+#define PIN_OUTPUT PIN_ROWS
+#define OUTPUT_PINMASK ROW_PINMASK
+#define OUTPUT_COUNT ROW_COUNT
+
+//Scanning port (cols)
+#define PORT_INPUT PORT_COLS
+#define DDR_INPUT DDR_COLS
+#define PIN_INPUT PIN_COLS
+#define INPUT_PINMASK COL_PINMASK
+#define INPUT_COUNT COL_COUNT
+
+#endif
 
 
 // AD01: lower two bits of device address
