fce6f1957a7a28c65197e557fd1087ff35a54006
Jesse Vincent <jesse@keyboard.io>
2018-04-09 12:37:17 -0700

    Start of an actual test harness to test all our debouncers

--- tools/debounce_test/run_testcase.pl
+++ tools/debounce_test/run_testcase.pl
@@ -5,18 +5,34 @@
 use warnings;
 use strict;
 
-my $datafile = shift @ARGV;
+my @testcases = `ls testcases/*`;
 
 my @debouncers = `ls ./debounce-*`;
 
+map {chomp} @testcases;
 map { chomp} @debouncers;
 
-run_debouncer('Source data', $debouncers[0], $datafile,'i');
+my $test_num =1;
 
+#run_debouncer('Source data', $debouncers[0], $datafile,'i');
+
+for my $test (@testcases) {
 for my $debouncer (@debouncers) {
-	my $title = $debouncer;
-	$title =~ s/^..debounce-//;
-	run_debouncer ($title, $debouncer, $datafile, '');
+	my $presses = -1;
+	my $metadata = `grep  PRESSES: $test`;
+	if ($metadata =~ /PRESSES:\s*(\d*)/ ){
+		$presses = $1;
+	}
+	my $title = `grep TITLE: $test `;
+	$title =~ s/^.*?TITLE://;
+	chomp($title);
+	my $count = run_debouncer ($title, $debouncer, $test, 'c');
+	if ($count == $presses) {
+		print "ok ". $test_num++. " - $title $debouncer found $presses presses\n";
+	} else {
+		print "not ok ". $test_num++ ." - $title $debouncer found $count presses but expected $presses\n";
+	}
+}
 }
 
 sub run_debouncer {
@@ -24,6 +40,7 @@ sub run_debouncer {
 	my $debouncer = shift;
 	my $data = shift;
 	my $arg = shift;
-	printf("%-40s",$title);
-	system("$debouncer $arg < $data");
+	my $result = `$debouncer $arg < $data`;
+	chomp($result);
+	return $result;
 }
