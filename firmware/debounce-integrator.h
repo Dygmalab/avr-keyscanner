3a204f7904ad2bfe4027ed21b189699dcadb5b07
Jesse Vincent <jesse@keyboard.io>
2018-04-19 12:02:27 -0700

    wip - fails some tests:
    ./debounce-integrator                   : OK 251  FAIL 11
    ./debounce-integrator                   : Failed: testcases/bad-manual-samples/13-chattering-o-2-more-presses, testcases/bad-manual-samples/13-chattering-o-4-more-presses-2, testcases/bad-manual-samples/13-key-o-5-presses, testcases/jays-attiny-detect-logic-i/key-i-2----5-presses-slow.data, testcases/jays-attiny-detect-logic-i/key-i-2----5-presses-slow.data.orig, testcases/jays-attiny-detect-logic/key-i-1--1-presses-slow.data, testcases/jays-attiny-detect-logic/key-i-3--5-presses-slow.data, testcases/jay-s/key-i--10-presses-slow.data, testcases/synthetic/04.7-seven-ms-gap-in-middle, testcases/synthetic/04.8-eight-ms-gap, testcases/synthetic/04.9-nine-ms-gap

--- firmware/debounce-integrator.h
+++ firmware/debounce-integrator.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <stdio.h>
 #include <stdint.h>
 #include "keyscanner.h"
 
@@ -12,9 +12,8 @@
 #define PRESCALER
 #define MS_TO_CYCLES(ms)  ((int) ms * (1/F_CPU ) *  KEYSCAN_INTERVAL_DEFAULT * PRESCALER
 
-#define LOCK_OUT_CYCLES 0
 
-static int8_t debounce_integrator_ceiling = 8 ; // ~4ms - this means that a keypress that's 4ms long needs a 4ms gap afterward
+static int8_t debounce_integrator_ceiling = 18 ; // ~4ms - this means that a keypress that's 4ms long needs a 4ms gap afterward
 static int8_t debounce_toggle_on_threshold = 3; // ~1ms
 static int8_t debounce_integrator_floor = 0;
 static int8_t debounce_toggle_off_threshold =  0;
@@ -28,6 +27,7 @@ typedef struct {
     int8_t counters[8];
     int8_t key_locked_out[8];
     uint8_t state;  // debounced state
+    uint8_t last_state; 
 } debounce_t;
 
 
@@ -58,39 +58,30 @@ static uint8_t debounce(uint8_t sample, debounce_t *debouncer) {
 
     // Scan each pin from the bank
     for(int8_t i=0; i< COUNT_INPUT; i++) {
-	if (debouncer->key_locked_out[i] > 0) {
-		// If the lockout is running, then force the counter to keep running, even if it goes outside the limits
-		debouncer->key_locked_out[i]--;
-		// If, during the lockout period we get even a single jitter
-		// of 'same state as before', it means there's chatter, so we 
-		// extend the lockout period
-		if ( !(sample & _BV(i))  &&  (debouncer->state & _BV(i))) {
-    				debouncer->key_locked_out[i] += 1; //LOCK_OUT_CYCLES;
-
-			
-		}
-    		(debouncer->state & _BV(i)) ? debouncer->counters[i]++ : debouncer->counters[i]--;
-		continue;
-	}
+	    if(i==0) 
+    //printf("Counter: %3d - ",debouncer->counters[i]);
         if (sample & _BV(i)) {
     	    if (debouncer->counters[i] < debounce_integrator_ceiling) {
                     debouncer->counters[i]++;
                	    if (debouncer->counters[i] == debounce_toggle_on_threshold &&	
     	      	    	debouncer->state ^ _BV(i) ) {
-    				debouncer->key_locked_out[i] = 10; //LOCK_OUT_CYCLES;
-               			changes |= _BV(i);
-    	    		}
+               		changes |= _BV(i);
+    	    	    }
+			if (debouncer->counters[i] > 2 ) 
+				debouncer->counters[i] += 13;
 	    }
         } else if ( debouncer->counters[i] > debounce_integrator_floor )  {
             debouncer->counters[i]--;
             if (debouncer->counters[i] == debounce_toggle_off_threshold &&
 	      	debouncer->state & _BV(i) ) {
-		debouncer->key_locked_out[i] =2 ; //LOCK_OUT_CYCLES;
             	changes |= _BV(i);
+		//debouncer->counters[i] = -18;
 	    }
+	    
         }
 
     }
+    debouncer->last_state = sample;
     debouncer->state ^= changes;
 
     return changes;
