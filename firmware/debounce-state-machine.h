ca8737f74a55a2eb8bf7e3dcb9b47faf4d004543
Jesse Vincent <jesse@keyboard.io>
2018-04-23 23:30:44 -0700

    rename  state to phase

--- firmware/debounce-state-machine.h
+++ firmware/debounce-state-machine.h
@@ -11,7 +11,7 @@
 
 
 typedef struct {
-    uint8_t lifecycle;
+    uint8_t phase;
     uint8_t ticks;
     uint8_t timer;
     uint8_t chatters;
@@ -24,80 +24,80 @@ typedef struct {
 
 
 
-enum lifecycle_states { OFF, TURNING_ON, LOCKED_ON, ON, TURNING_OFF, LOCKED_OFF};
+enum lifecycle_phases { OFF, TURNING_ON, LOCKED_ON, ON, TURNING_OFF, LOCKED_OFF};
 
 typedef struct {
     uint8_t
-    next_state: 3,
-                unexpected_data_state: 3,
+    next_phase: 3,
+                unexpected_data_phase: 3,
                 expected_data:1,
                 unexpected_data_is_chatter: 1;
     int8_t regular_timer;
     int8_t chattering_switch_timer;
-} lifecycle_state_t;
+} lifecycle_phase_t;
 
 
 
-lifecycle_state_t lifecycle[] = {
+lifecycle_phase_t lifecycle[] = {
     {
         // OFF
-        .next_state = TURNING_ON,
+        .next_phase = TURNING_ON,
         .expected_data = EXPECTED_ON,
-        .unexpected_data_state = OFF,
+        .unexpected_data_phase = OFF,
         .regular_timer = 0,
         .unexpected_data_is_chatter = 0,
         .chattering_switch_timer = 0
     },
     {
         // TURNING_ON
-        .next_state = LOCKED_ON,
+        .next_phase = LOCKED_ON,
         .expected_data = EXPECTED_ON,
-        .unexpected_data_state = OFF,
+        .unexpected_data_phase = OFF,
         .regular_timer = 1,
         .unexpected_data_is_chatter = 1,
         .chattering_switch_timer = 2
     },
     {
         // LOCKED_ON
-        .next_state = ON,
+        .next_phase = ON,
         .expected_data = EXPECTED_ON,
-        .unexpected_data_state = LOCKED_ON,
+        .unexpected_data_phase = LOCKED_ON,
         .regular_timer = 10,
         .unexpected_data_is_chatter = 1,
         .chattering_switch_timer = 15
     },
     {
         // ON
-        .next_state = TURNING_OFF,
+        .next_phase = TURNING_OFF,
         .expected_data = EXPECTED_OFF,
-        .unexpected_data_state = ON,
+        .unexpected_data_phase = ON,
         .regular_timer = 6,
         .unexpected_data_is_chatter = 0,
         .chattering_switch_timer = 35
     },
     {
         // TURNING_OFF
-        .next_state = LOCKED_OFF,
+        .next_phase = LOCKED_OFF,
         .expected_data = EXPECTED_OFF,
-        .unexpected_data_state = ON,
+        .unexpected_data_phase = ON,
         .regular_timer = 4,
         .unexpected_data_is_chatter = 1,
         .chattering_switch_timer = 26
     },
     {
         // LOCKED_OFF
-        .next_state = OFF,
+        .next_phase = OFF,
         .expected_data = EXPECTED_OFF,
-        .unexpected_data_state =  LOCKED_OFF,
+        .unexpected_data_phase =  LOCKED_OFF,
         .regular_timer = 30,
         .unexpected_data_is_chatter =1,
         .chattering_switch_timer = 30
     }
 };
 
-void transition_to_state(key_info_t *key_info, int8_t new_state) {
-    key_info->timer=(key_info->chatters ? lifecycle[new_state].chattering_switch_timer: lifecycle[new_state].regular_timer );
-    key_info->lifecycle= new_state;
+void transition_to_phase(key_info_t *key_info, int8_t new_phase) {
+    key_info->timer=(key_info->chatters ? lifecycle[new_phase].chattering_switch_timer: lifecycle[new_phase].regular_timer );
+    key_info->phase= new_phase;
     key_info->ticks=0;
 }
 
@@ -113,21 +113,21 @@ static uint8_t debounce(uint8_t sample, debounce_t *debouncer) {
         key_info_t *key_info	= debouncer->key_info+i;
         key_info->ticks++;
 
-	lifecycle_state_t current_phase = lifecycle[key_info->lifecycle];
+	lifecycle_phase_t current_phase = lifecycle[key_info->phase];
 
         // if we get the 'other' value during a locked window, that's gotta be chatter
         if ((sample & _BV(i)) != 
 			current_phase.expected_data) {
             key_info->chatters = key_info->chatters || current_phase.unexpected_data_is_chatter;
-            transition_to_state(key_info, current_phase.unexpected_data_state);
+            transition_to_phase(key_info, current_phase.unexpected_data_phase);
         }
 
         // 	do not act on any input during the locked off window
         if (key_info->ticks > key_info->timer) {
 
-            transition_to_state(key_info, current_phase.next_state);
+            transition_to_phase(key_info, current_phase.next_phase);
 
-            if ( key_info->lifecycle == LOCKED_ON  || key_info->lifecycle == LOCKED_OFF)
+            if ( key_info->phase == LOCKED_ON  || key_info->phase == LOCKED_OFF)
                 changes |= _BV(i);
         }
     }
