e079476822aede79f3db16436dde2f0932098e6b
Jesse Vincent <jesse@keyboard.io>
2018-04-24 18:28:16 -0700

    ./debounce-state-machine                : OK 290  FAIL 12
    (Now passing testcases/jays-attiny-detect-logic-i/key-i-2----5-presses-slow.data)
    
    ./debounce-state-machine                : Failed:
    	 testcases/bad-manual-samples/13-chattering-o-2-more-presses
    	 testcases/impossible-chatter/key-i-1--1-presses-slow.data
    	 testcases/synthetic/04.11-ms-gap
    	 testcases/synthetic/04.12-ms-gap
    	 testcases/synthetic/04.13-ms-gap
    	 testcases/synthetic/04.14-ms-gap
    	 testcases/synthetic/04.15-ms-gap
    	 testcases/synthetic/04.16-ms-gap
    	 testcases/synthetic/04.17-ms-gap
    	 testcases/synthetic/04.18-ms-gap
    	 testcases/synthetic/04.19-ms-gap
    	 testcases/synthetic/04.20-ms-gap

--- firmware/debounce-state-machine.h
+++ firmware/debounce-state-machine.h
@@ -65,7 +65,7 @@ lifecycle_phase_t lifecycle[] = {
         .unexpected_data_phase = TURNING_OFF,
         .regular_timer = 5,
         .unexpected_data_is_chatter = 0,
-        .chattering_switch_timer =55
+        .chattering_switch_timer =65
     },
     {
         // TURNING_OFF
@@ -74,7 +74,7 @@ lifecycle_phase_t lifecycle[] = {
         .unexpected_data_phase = ON,
         .regular_timer = 20,
         .unexpected_data_is_chatter = 1,
-        .chattering_switch_timer =80 
+        .chattering_switch_timer =95
     },
     {
         // LOCKED_OFF
