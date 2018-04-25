4658723ebe5b62c5739fc7726025e39316d907b4
Jesse Vincent <jesse@keyboard.io>
2018-04-25 15:31:05 -0700

    kill the 'current_phase' temporary variable. it only added a little visual clarity and there was potential confusion around when data got changed out from under it.

--- firmware/debounce-state-machine.h
+++ firmware/debounce-state-machine.h
@@ -214,13 +214,12 @@ static uint8_t debounce(uint8_t sample, debounce_t *debouncer) {
     for(int8_t i=0; i< COUNT_INPUT; i++) {
         key_info_t *key = debouncer->key_info+i;
 
-        lifecycle_phase_t current_phase = lifecycle[key->phase];
 
-        if (!!(sample & _BV(i)) != current_phase.expected_data) {
+        if (!!(sample & _BV(i)) != lifecycle[key->phase].expected_data) {
             // if we get the 'other' value during a locked window, that's gotta be chatter
-            if (key->phase != current_phase.unexpected_data_phase) {
+            if (key->phase != lifecycle[key->phase].unexpected_data_phase) {
 
-                key->phase = current_phase.unexpected_data_phase;
+                key->phase = lifecycle[key->phase].unexpected_data_phase;
                 key->ticks= lifecycle[key->phase].timer;
 
                 continue;
@@ -231,9 +230,10 @@ static uint8_t debounce(uint8_t sample, debounce_t *debouncer) {
 	if (key->ticks-->0) {
 		continue;
 	}
-        if (  (key->phase != current_phase.next_phase)) {
 
-            key->phase = current_phase.next_phase;
+        if (  (key->phase != lifecycle[key->phase].next_phase)) {
+
+            key->phase = lifecycle[key->phase].next_phase;
             key->ticks = lifecycle[key->phase].timer;
 
             changes |= _BV(i) & lifecycle[key->phase].change_output_on_expected_transition;
