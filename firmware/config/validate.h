a942457ef54552f09a1a20e4479214075d97bdcb
matt venn <matt@mattvenn.net>
2018-05-07 13:04:01 +0200

    validate file

---
+++ firmware/config/validate.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#if !defined(PORT_ROWS)
+# error PORT_ROWS not configured for target
+#endif
+
+#if !defined(DDR_ROWS)
+# error DDR_ROWS not configured for target
+#endif
+
+#if !defined(PIN_ROWS)
+# error PIN_ROWS not configured for target
+#endif
+
+#if !defined(PORT_COLS)
+# error PORT_COLS not configured for target
+#endif
+
+#if !defined(DDR_COLS)
+# error DDR_COLS not configured for target
+#endif
+
+#if !defined(PIN_COLS)
+# error PIN_COLS not configured for target
+#endif
