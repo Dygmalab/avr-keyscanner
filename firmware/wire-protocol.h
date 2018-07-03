8e6650ca4efaf3b86f89b67fbe13ff74553afc92
matt venn <matt@mattvenn.net>
2018-07-03 10:42:09 +0200

    faster read times

--- firmware/wire-protocol.h
+++ firmware/wire-protocol.h
@@ -24,7 +24,7 @@ typedef union {
 extern uint8_t led_spi_frequency;
 
 // Default to a minimum of 1.6ms between reads
-#define KEYSCAN_INTERVAL_DEFAULT 50
+#define KEYSCAN_INTERVAL_DEFAULT 15
 
 // I²C driver functions
 void twi_data_received( uint8_t *buf, uint8_t bufsiz);
