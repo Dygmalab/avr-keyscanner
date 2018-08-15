8bb70f6f676cb20f35cfc9a7c2ffc0247a1618c7
matt venn <matt@mattvenn.net>
2018-08-15 14:15:19 +0200

    average cc read

--- firmware/main.c
+++ firmware/main.c
@@ -6,7 +6,7 @@
 #include <stdint.h>
 #include "adc.h"
 
-//#define DETECT_ADC
+#define DETECT_ADC
 
 uint8_t red[3] = { 255, 0, 0 };
 uint8_t grn[3] = { 0, 255, 0 };
@@ -18,14 +18,26 @@ static inline void setup(void) {
     keyscanner_init();
 
     #ifdef DETECT_ADC
+    // set CC pin to input
+    SET_INPUT(DDRA,0);
 
     // if dpf is providing power to us
-    while(read_adc(ADC_CC) > 40)
+    uint16_t average = 0;
+    while(true)
     {
-        led_set_all_to(red); 
-        _delay_ms(100);
+        for(int i = 0; i < 10; i ++)
+            average += read_adc(ADC_CC);
+        average /= 10;
+
+
+        if(average > 40)
+        {
+            led_set_all_to(red); 
+            _delay_ms(100);
+        }
+        else
+            break;
     }
-    led_set_all_to(off); 
     #endif
     twi_init();
 }
