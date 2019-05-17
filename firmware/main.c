7ec1966fe51411a49b8f8f64fc94d37e32bf262a
matt venn <matt@mattvenn.net>
2019-05-17 15:26:46 +0200

    hack to fix the pullup ANSI/ISO problem

--- firmware/main.c
+++ firmware/main.c
@@ -40,12 +40,17 @@ static inline void setup(void) {
     }
     #endif
 
-    // ansi iso reading
-    // set input
-    SET_INPUT(DDRB,1);
-    // pull down
+    // ansi iso reading - up to v4.9 pcbs use ANSI pullup, ISO floating
+    // but chip doesn't have configurable pulldowns, only pullups
+    // set pin to be low output first, to drain any voltage on pin's capacitance
+    SET_OUTPUT(DDRB,1);
     LOW(PORTB,1);
-    // read it: 0 = ISO, 2 = ANSI
+
+    // then set it input
+    SET_INPUT(DDRB,1);
+    // leave enough time full the pullup to change the voltage on the pin
+    asm("nop");
+    // then read: 0 = ISO, 2 = ANSI
     ansi_iso = (PINB & _BV(1)) == 2 ? ANSI : ISO;
     
     // initialise twi
